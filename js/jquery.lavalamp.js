9a2fccc184a27eae6299d6727c0794cb2c856276
Richard Hung <richardhung111@gmail.com>
2015-08-19 20:19:44 -0700

    animate on focus

--- js/jquery.lavalamp.js
+++ js/jquery.lavalamp.js
@@ -1,4 +1,4 @@
-/**
+/*!
  * Lava Lamp
  * http://lavalamp.magicmediamuse.com/
  *
@@ -7,7 +7,7 @@
  * http://www.magicmediamuse.com/
  *
  * Version
- * 1.0.7
+ * 1.0.8
  * 
  * Copyright (c) 2014 Richard Hung.
  * 
@@ -18,6 +18,8 @@
 
 
 (function($) {
+	
+	'use strict';
 	
 	var methods = {
 		init : function(settings) {
@@ -33,17 +35,17 @@
 				updateTime:  100,       // Time between update checks
 				enableHover: true,      // lavalamp moves with hover instead of click
 				delayOn:     0,         // Delay time on hover
-				delayOff:    0          // Delay time off hover
+				delayOff:    0,         // Delay time off hover
+				enableFocus: false,     // Animate on keyboard focus
+				deepFocus:   false      // Animate on decendent focus 
 			}; // End options
 			
 			// Override default options
-			var settings = $.extend({}, defaultSettings, settings);
+			settings = $.extend({}, defaultSettings, settings);
 			
 			return this.each(function(){
 				
 				// Get the options
-				var easing      = settings.easing;
-				var duration    = settings.duration;
 				var margins     = settings.margins;
 				var setOnClick  = settings.setOnClick;
 				var activeObj   = settings.activeObj;
@@ -52,6 +54,8 @@
 				var enableHover = settings.enableHover;
 				var delayOn     = settings.delayOn;
 				var delayOff    = settings.delayOff;
+				var enableFocus = settings.enableFocus;
+				var deepFocus   = settings.deepFocus;
 				
 				// Set variables
 				var list   = $(this);
@@ -65,9 +69,9 @@
 				
 				// Set variables to object
 				list.data({
-					active:   active,
-					isAnim:   false,
-					settings: settings
+					lavalampActive: active,
+					isAnim:         false,
+					settings:       settings
 				});
 				
 				// Create basic structure
@@ -105,30 +109,62 @@
 					left:   l
 				});
 				
-				var onHover = false;
+				var onHover = false; // check if item is still hovered after delay
+				var enAnim  = true; // temporarily disable animations when in focus
 				lavalampEnter = function() {
 					var des = $(this);
 					onHover = true;
 					
 					// delay and animate
 					setTimeout(function() {
-						if (onHover) {
+						if (onHover && enAnim) {
 							list.lavalamp('anim',des);
 						}
 					},delayOn);
 					
 				}; // end mousenter
-				lavalampLeave = function() {
-					var des = list.data('active');
+				lavalampLeave = function(des) {
+					des = list.data('lavalampActive');
 					onHover = false;
 					
 					// delay and animate
 					setTimeout(function() {
-						if (!onHover) {
+						if (!onHover && enAnim) {
 							list.lavalamp('anim',des);
 						}
 					},delayOff);
+					
 				}; // end mouseleave
+
+				focusIn = function() {
+					var des = $(this);
+					
+					// Back out of deep decendents
+					if (!des.hasClass('lavalamp-item')) {
+						des = des.parents('.lavalamp-item');
+					}
+					
+					enAnim = false;
+					
+					// delay and animate
+					setTimeout(function() {
+						list.lavalamp('anim',des);
+					},delayOn);
+					console.log('in');
+					
+				}; // end focus in
+				focusOut = function() {
+					enAnim = true;
+					
+					var des = list.data('lavalampActive');
+					console.log(des);
+					
+					// delay and animate
+					setTimeout(function() {
+						list.lavalamp('anim',des);
+					},delayOff);
+					console.log('out');
+				}; // end focus out
 				
 				// items.hover(enter, leave);
 					
@@ -137,11 +173,20 @@
 					list.on('mouseenter','.lavalamp-item',lavalampEnter);
 					list.on('mouseleave','.lavalamp-item',lavalampLeave);
 				}
+				if (enableFocus) {
+					list.on('focusin','.lavalamp-item',focusIn);
+					list.on('focusout','.lavalamp-item',focusOut);
+				}
+				if (deepFocus) {
+					list.on('focusin','.lavalamp-item *',focusIn);
+					list.on('focusout','.lavalamp-item *',focusOut);
+				}
+								
 								
 				if (setOnClick) {
 					items.click(function() {
 						active = $(this);
-						list.data('active',active).lavalamp('update');
+						list.data('lavalampActive',active).lavalamp('update');
 					});
 				} // End set on click
 				
@@ -164,12 +209,22 @@
 				var settings    = list.data('settings');
 				var items       = list.children('.lavalamp-item');
 				var enableHover = settings.enableHover;
+				var enableFocus = settings.enableFocus;
+				var deepFocus   = settings.deepFocus;
 				var autoUpdate  = settings.autoUpdate;
 				
 				// Unbind the plugin effect
 				if (enableHover) {
 					list.off('mouseenter', '.lavalamp-item', lavalampEnter);
 					list.off('mouseleave', '.lavalamp-item', lavalampLeave);
+				}
+				if (enableFocus) {
+					list.off('focusin','.lavalamp-item',focusIn);
+					list.off('focusout','.lavalamp-item',focusOut);
+				}
+				if (deepFocus) {
+					list.off('focusin','.lavalamp-item *',focusIn);
+					list.off('focusout','.lavalamp-item *',focusOut);
 				}
 				
 				// Remove CSS
@@ -194,8 +249,7 @@
 			return this.each(function(){
 				var list   = $(this);
 				var items  = list.children(':not(.lavalamp-object)');
-				var active = list.data('active');
-				var obj    = list.children('.lavalamp-object');
+				var active = list.data('lavalampActive');
 				
 				// reset list objects
 				items.addClass('lavalamp-item').css({
@@ -218,8 +272,6 @@
 			var obj = list.children('.lavalamp-object');
 			
 			var w  = destination.outerWidth(margins);
-
-
 			var h  = destination.outerHeight(margins);
 			var t  = destination.position().top;
 			var l  = destination.position().left;
@@ -261,7 +313,7 @@
 	}; // End plugin
 	
 	// Create outer variables
-	var lavalampEnter, lavalampLeave;
+	var lavalampEnter, lavalampLeave, focusIn, focusOut;
 		
 })(jQuery); 
 
